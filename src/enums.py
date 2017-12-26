ac8186a1b64011dc2bb53f5d346c8ed017e20f98
Eli Sohl <elisohl@gmail.com>
2017-12-26 03:52:26 -0500

    enums to support dispatcher

--- src/enums.py
+++ src/enums.py
@@ -1,11 +1,14 @@
 from enum import Enum
 
 
-NodeStateKeys = Enum("NodeStateKeys", "ROLE")
-ROLE = NodeStateKeys.ROLE
-
-NodeInfoKeys = Enum("NodeInfoKeys", "ID LISTEN_PORT MAX_VERSION")
-ID, LISTEN_PORT, MAX_VERSION = NodeInfoKeys.ID, NodeInfoKeys.LISTEN_PORT, NodeInfoKeys.MAX_VERSION
+NodeStateKeys = Enum("NodeStateKeys", "ROLE STATE LAST_ACTIVE")
+ROLE, STATE, LAST_ACTIVE = NodeStateKeys.ROLE, NodeStateKeys.STATE, NodeStateKeys.LAST_ACTIVE
 
 NoiseProtoRoles = Enum("NoiseProtoRoles", "INITIATOR RESPONDER")
 INITIATOR, RESPONDER = NoiseProtoRoles.INITIATOR, NoiseProtoRoles.RESPONDER
+
+NodeCnxnStates = ENUM("NodeCnxnStates", "DISCONNECTED CONNECTING CONNECTED")
+DISCONNECTED, CONNECTING, CONNECTED = NodeCnxnStates.DISCONNECTED, NodeCnxnStates.CONNECTING, NodeCnxnStates.CONNECTED
+
+NodeInfoKeys = Enum("NodeInfoKeys", "ID LISTEN_PORT MAX_VERSION")
+ID, LISTEN_PORT, MAX_VERSION = NodeInfoKeys.ID, NodeInfoKeys.LISTEN_PORT, NodeInfoKeys.MAX_VERSION
