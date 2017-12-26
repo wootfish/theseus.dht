96b080707cd95f2338e056c6d5beca975552405c
Eli Sohl <elisohl@gmail.com>
2017-12-25 19:45:13 -0500

    added NodeInfoKeys enum

--- src/enums.py
+++ src/enums.py
@@ -4,5 +4,8 @@ from enum import Enum
 NodeStateKeys = Enum("NodeStateKeys", "ROLE")
 ROLE = NodeStateKeys.ROLE
 
+NodeInfoKeys = Enum("NodeInfoKeys", "ID LISTEN_PORT MAX_VERSION")
+ID, LISTEN_PORT, MAX_VERSION = NodeInfoKeys.ID, NodeInfoKeys.LISTEN_PORT, NodeInfoKeys.MAX_VERSION
+
 NoiseProtoRoles = Enum("NoiseProtoRoles", "INITIATOR RESPONDER")
 INITIATOR, RESPONDER = NoiseProtoRoles.INITIATOR, NoiseProtoRoles.RESPONDER
