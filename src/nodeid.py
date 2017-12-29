85c2b6ab0b5b86db24e23ced46c629ca980205a0
Eli Sohl <elisohl@gmail.com>
2017-12-28 23:23:08 -0500

    Update nodeid.py

--- src/nodeid.py
+++ src/nodeid.py
@@ -5,22 +5,30 @@ from .enums import UNSET, LOW, MEDIUM, HIGH, CRITICAL
 
 class NodeID:
     def __init__(self, node_id, verify=True, priority=UNSET):
-        # pass node_id = None to generate a random ID with a current timestamp.
+        # pass node_id=None to generate a random ID with a current timestamp.
         # priority defaults to CRITICAL if node_id is None, unless a value
         # other than UNSET is passed in.
 
         self.node_id = node_id
-        self.address = node_id[0]
-        self.preimage = node_id[1]
         self.priority = priority
 
-        self.check_result = None
+        try:
+            self.address = node_id[0]
+            self.preimage = node_id[1]
+        except:
+            self.address = None
+            self.preimage = None
+
+        self.addr_check_retval = None
         self.on_check = Deferred()
 
         if node_id is None:
             self.generate_address()
         elif verify:
             self.verify_address()
+
+    def __repr__(self):
+        return "NodeID(({}, {}))".format(self.address, self.preimage)
 
     def set_priority(self, new_priority):
         if new_priority.value > self.priority.value:
