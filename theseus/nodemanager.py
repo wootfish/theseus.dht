f1948142bc4244bb9ed4ec01a493d217c192bb89
Eli Sohl <elisohl@gmail.com>
2018-01-08 04:07:18 -0500

    NodeManagerService.__init__ now can take node IDs

--- theseus/nodemanager.py
+++ theseus/nodemanager.py
@@ -8,11 +8,20 @@ class NodeManagerService(MultiService):
     log = Logger()
     num_nodes = 5
 
-    def __init__(self, num_nodes=None):
+    def __init__(self, num_nodes=None, node_ids=None):
         super().__init__()
 
         if num_nodes is not None:
             self.num_nodes = num_nodes
+        elif node_ids is not None:
+            self.num_nodes = len(node_ids)
 
-        for _ in range(self.num_nodes):
-            self.addService(NodeService(self))
+        if node_ids is None:
+            for _ in range(self.num_nodes):
+                self.addService(NodeService(self))
+        else:
+            if num_nodes is not None and len(node_ids) != num_nodes:
+                raise Exception("len(node_ids) and num_nodes disagree")
+
+            for node_id in node_ids:
+                self.addService(NodeService(self, node_id))
