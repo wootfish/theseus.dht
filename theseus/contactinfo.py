5d1a1bb30d1b5470df12787f2c29325a76b849a2
Eli Sohl <elisohl@gmail.com>
2018-02-11 05:38:21 -0500

    improvements to ContactInfo & node tracking

--- theseus/contactinfo.py
+++ theseus/contactinfo.py
@@ -1,19 +1,20 @@
-from twisted.internet.address import IPv4Address
-
 class ContactInfo:
-    def __init__(self, listen_addr, remote_key, node_id):
-        self.listen_addr = listen_addr
+    def __init__(self, remote_host, remote_port, remote_key=None, node_id=None):
+        self.remote_host = remote_host
+        self.remote_port = remote_port
         self.key = remote_key
         self.node_id = node_id
 
     def __hash__(self):
-        return hash((self.addr, self.key, self.node_id))
+        return hash((self.remote_host, self.remote_port, self.key, self.node_id))
 
     def __eq__(self, other):
         if not issubclass(other.__class__, ContactInfo):
             return False
 
-        return self.node_id == other.node_id and self.addr == other.addr and self.key == other.key
+        return (self.remote_host == other.remote_host and
+                self.remote_port == other.remote_port and
+                self.key == other.key and self.node_id == other.node_id)
 
     def __repr__(self):
         return "ContactInfo({}, {}, {})".format(self.addr, self.key, self.node_id)
--- theseus/nodetracker.py
+++ theseus/nodetracker.py
@@ -1,21 +1,24 @@
+from twisted.internet import reactor
+from twisted.internet.protocol import Factory
+
 import time
 
 
-class NodeState:
-    role = None
+class NodeState(Factory):
+    reactor = reactor  # kind of wild how this isn't a no-op
+
     state = None
     host = None
     cnxn = None
-    last_active = None
-    info = {}
+    #last_active = None
 
     def __init__(self):
-        self.last_active = time.monotonic()
+        #self.last_active = time.monotonic()
+        self.info = {}
 
     @classmethod
-    def asInitiator(cls, contact_info):
+    def fromContact(cls, contact_info):
         instance = cls()
-        instance.role = INITIATOR
         instance.state = DISCONNECTED
         instance.host = contact_info.listen_addr.host
         instance.info[LISTEN_PORT] = contact_info.listen_addr.port
@@ -23,7 +26,7 @@ class NodeState:
         return instance
 
     @classmethod
-    def asResponder(cls, cnxn):
+    def fromCnxn(cls, cnxn):
         instance = cls()
         instance.role = RESPONDER
         instance.state = CONNECTING
@@ -31,8 +34,20 @@ class NodeState:
         instance.cnxn = cnxn
         return instance
 
+    def buildProtocol(self, addr):
+        # TODO we need to figure out what the best way is for the NodeTracker
+        # to find out about p here and add it to its self.by_cnxn dict
+        # we'll need to either give this object a ref back to the tracker
+        # or else move some factory duties to the tracker (which might be a
+        # good idea anyway bc it's still sort of up in the air how we'll handle
+        # _listening_)
+        p = NoiseFactory(INITIATOR, Factory.forProtocol(DHTProtocol)).buildProtocol(addr)
+        p.context = NoiseSettings(remote_static=self.info[NODE_KEY])
+        return p
+
     def connect(self):
-        ...
+        endpoint = TCP4ClientEndpoint(self.reactor, self.host, self.info[LISTEN_PORT])
+        return endpoint.connect(self)
 
     def disconnect(self):
         ...
@@ -51,11 +66,18 @@ class NodeTracker:
     def __init__(self, parent):
         self.parent = parent
 
+        self.by_cnxn = {}
+        self.by_contact = {}
+
     def register(self, contact_info):
-        ...
+        if contact_info not in self.by_contact:
+            new_state = NodeState.fromContact(contact_info)
+            new_state.client_factory = self.client_factory
+            self.by_contact[contact_info] = new_state
+        return self.by_contact[contact_info]
 
-    def byCnxn(self, cnxn):
-        ...
+    def getByCnxn(self, cnxn):
+        return self.by_cnxn.get(cnxn)
 
-    def byContact(self, contact_info):
-        ...
+    def getByContact(self, contact_info):
+        return self.by_contact.get(contact)
