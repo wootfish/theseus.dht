d487364a0f9bd0453841c72a382ee60e74313384
Eli Sohl <elisohl@gmail.com>
2018-04-19 00:37:55 -0700

    improved get_coverage.sh output formatting

--- scripts/get_coverage.sh
+++ scripts/get_coverage.sh
@@ -7,7 +7,7 @@ if covered+uncovered == 0:
     print('No lines given!')
 else:
     n = round(100*covered/(covered+uncovered), 3)
-    print('{n}% - {x} covered, {y} uncovered'.format(n=n, x=covered, y=uncovered))
+    print('{n}%'.format(n=n).ljust(7), '- {x} covered,'.format(x=covered).ljust(14), '{y} uncovered'.format(y=uncovered))
 "
 
 cd /tmp
@@ -23,7 +23,8 @@ total_uncovered=0
 for fname in $(ls | grep theseus | grep -v "test\|__init__"); do
     uncovered=$(cat $fname | grep '^>>>>>>' | wc -l)
     covered=$(cat $fname | grep -v '^>>>>>>' | wc -l)
-    echo -n "$fname - "
+    #echo -n "$fname - "
+    python -c "print('$fname'.ljust(28), end='')"
     echo $covered $uncovered | python3 -c "$python_script"
     total_covered=$(( total_covered + covered ))
     total_uncovered=$(( total_uncovered + uncovered ))
