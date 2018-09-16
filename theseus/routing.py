7cf1f6aef2c2470a87bb8d15ba4cad9b9263f785
Eli Sohl <elisohl@gmail.com>
2018-09-15 22:28:26 -0700

    rewrote routing table, added basic unit tests

--- theseus/routing.py
+++ theseus/routing.py
@@ -1,143 +1,149 @@
-from twisted.logger import Logger
+from twisted.logger import Logger 
 
-from .enums import ADDRS
 from .constants import k, L
 
 
 class RoutingTable:
-    """
-    Maintains a Kademlia-style routing table.
-    The actual entities stored are ContactInfo objects.
-    The buckets storing these are determined by NodeAddrs.
-    A ContactInfo's NodeAddrs are retrieved through RoutingTable.get_node_addrs.
-    """
-
-    log = Logger()
-
-    # rebind relevant constants so we can reference them as members of bound
-    # methods' self namespace rather than the global namespace
-
     k = k
     L = L
 
-    def __init__(self, local_peer):
+    class Entry:
+        # TODO these need to be singletons (or to have an equality check based on their data field contents)
+        def __init__(self, contact_info, node_addr):
+            self.contact_info = contact_info
+            self.node_addr = node_addr
+
+        def __repr__(self):
+            return "RoutingTable.Entry({}, {})".format(self.contact_info, self.node_addr)
+
+    class Bucket:
+        def __init__(self, lower, upper, k):
+            self.lower = lower
+            self.upper = upper
+            self.k = k
+
+            self.contents = []
+            self.left_child = None
+            self.right_child = None
+
+        def insert(self, entry, local_addrs=None):
+            # TODO if insert succeeds, add a timer to clean up the node ID when it expires
+            # NOTE: returns True if insert succeeds _or_ entry is already in table
+
+            local_addrs = [node_addr for node_addr in local_addrs or [] if self.covers(node_addr.addr)]
+
+            if self.contents is None:
+                # bucket is split
+                if self.left_child.covers(entry.node_addr.addr):
+                    return self.left_child.insert(entry, local_addrs)
+                return self.right_child.insert(entry, local_addrs)
+
+            if len(self.contents) < self.k:
+                # bucket has room
+                if entry not in self.contents:
+                    self.contents.append(entry)
+                return True
+
+            if local_addrs:
+                # bucket has no room, but can be split
+                #print("Splitting bucket {} - {} due to node addrs {}".format(self.lower, self.upper, local_addrs))
+                self.split()
+                return self.insert(entry, local_addrs)
+
+            return False
+
+        def split(self):
+            contents = self.contents
+            self.contents = None
+
+            bisector = (self.lower + self.upper) // 2
+            self.left_child = RoutingTable.Bucket(self.lower, bisector, self.k)
+            self.right_child = RoutingTable.Bucket(bisector + 1, self.upper, self.k)
+
+            for entry in contents:
+                self.insert(entry)
+
+        def covers(self, addr: bytes):
+            return self.lower <= RoutingTable.bytes_to_int(addr) <= self.upper
+
+        def query(self, addr, lookup_size):
+            addr_int = self.bytes_to_int(addr)
+
+            if self.contents:
+                if len(self.contents) <= lookup_size:
+                    return self.contents
+                return sorted(
+                        self.contents,
+                        key=lambda entry: addr_int ^ self.bytes_to_int(entry.addr)
+                        )[:lookup_size]
+
+            if self.left_child.covers(addr):
+                closer, further = self.left_child, self.right_child
+            else:
+                closer, further = self.right_child, self.left_child
+
+            results = closer.query(addr, lookup_size)
+            num_results = len(results)
+            if num_results < lookup_size:
+                results += further.query(addr, lookup_size - num_results)
+            return results
+
+        def get_contents(self):
+            if self.contents:
+                return self.contents
+            return self.left_child.get_contents() + self.right_child.get_contents()
+
+        # def show(self, indent=0):
+        #     # convenience function for troubleshooting
+        #     spacing = ' '*4*indent
+
+        #     lower = '0x'+hex(self.lower)[2:].rjust(RoutingTable.L//4, '0')
+        #     upper = '0x'+hex(self.upper)[2:].rjust(RoutingTable.L//4, '0')
+        #     print(spacing, end='')
+        #     print(lower, '-', upper)
+
+        #     if self.contents is None:
+        #         self.left_child.show(indent+1)
+        #         self.right_child.show(indent+1)
+        #     else:
+        #         if len(self.contents) == 0:
+        #             print(spacing, end='')
+        #             print("Empty")
+        #         for entry in self.contents:
+        #             print(spacing, end='')
+        #             print(entry)
+
+
+    def __init__(self, local_peer=None):
         self.local_peer = local_peer
-        self.buckets = {(0, 2**self.L - 1): set()}
+        self.root = self.Bucket(0, 2**self.L - 1, self.k)
 
-    def __contains__(self, contact):
-        for bucket in self.buckets.values():
-            if contact in bucket:
-                return True
-            return False
+    def query(self, addr, lookup_size=None):
+        return self.root.query(addr, lookup_size or self.k)
 
-    def _bucketLookup(self, node_address):
-        addr_int = self.bytes_to_int(node_address)
-        for bucket in self.buckets:
-            if bucket[0] <= addr_int <= bucket[1]:
-                return bucket
-        raise Exception("node_address out of bounds")
+    def insert(self, contact_info, node_addr):
+        entry = self.Entry(contact_info, node_addr)
+        return self.root.insert(entry, self._get_local_addrs())
 
-    def _bucketIsSplitCandidate(self, bucket):
-        #if not self.local_peer:
-        #    return False
-        for node_addr in self.local_peer.node_addrs:
-            if bucket[0] <= self.bytes_to_int(node_addr.node_addr) <= bucket[1]:
-                return True
-        return False
+    def reload(self, local_addrs=None):
+        # to be called after a local addr is replaced
+        if local_addrs is None:
+            local_addrs = self._get_local_addrs()
 
-    def _bucketSplit(self, bucket):
-        # returns True if split was successful, False if it wasn't allowed
-        if bucket[0] == bucket[1]:
-            self.log.warn("Weird edge case encountered: Failed to split bucket {bucket}", bucket=bucket)
-            return False
+        contents = self.root.get_contents()  # TODO would there be any reason to shuffle this? just so that it's not predetermined which IDs from cut buckets survive? is there any good reason to want that?
+        self.root = self.Bucket(0, 2**self.L - 1, self.k)
+        for entry in contents:
+            self.root.insert(entry, local_addrs)
 
-        if not self._bucketIsSplitCandidate(bucket):
-            return False
-
-        bisector = (bucket[0] + bucket[1]) // 2
-        lower = bucket[0], bisector
-        upper = bisector+1, bucket[1]
-        self.buckets[lower] = {}
-        self.buckets[upper] = {}
-
-        for listen_addr, node_addr in self.buckets.pop(bucket).items():
-            self._insert(listen_addr, node_addr)
-
-        self.log.info("Routing table bucket {bucket} split. Current table state: {pretty}", bucket=(hex(bucket[0]), hex(bucket[1])), pretty=self.pretty())
-        return True
-
-    def insert(self, contact_info):
-        """
-        Retrieves the node addrs associated with contact_info and tries to
-        insert contact_info to their associated buckets.
-        """
-
-        node_addrs = self.get_node_addrs(contact_info)
-        self.log.debug("Attempting to insert {contact} into routing table. (Addrs: {node_addrs})", contact=contact_info, node_addrs=node_addrs)
-
-        for node_addr in node_addrs:
-            self._insert(contact_info, node_addr)
-
-    def _insert(self, contact_info, node_addr):
-        bucket_key = self._bucketLookup(node_addr)
-        bucket = self.buckets[bucket_key]
-
-        if contact_info in bucket:
-            return
-
-        if len(bucket) <= self.k:
-            bucket.add(contact_info)
-            return
-
-        # bucket is full, but maybe we can split it & then retry the insert
-        if self._bucketSplit(bucket_key):
-            self._insert(contact_info, node_addr)
-
-    def discard(self, contact_info):
-        for bucket in self.buckets.values():
-            if contact_info in bucket:
-                bucket.remove(contact_info)
-
-    def query(self, target_addr):
-        return self.buckets[self._bucketLookup(target_addr)]
-
-    def pretty(self):
-        """
-        Returns a prettyprintable representation of the table's state. This
-        mostly means converting things to hex where appropriate and boiling off
-        some boilerplate.
-        """
-
-        pretty = {}
-        for bucket in self.table.buckets:
-            lower_padded = "0x" + hex(bucket[0])[2:].rjust(self.L//8, '0')
-            upper_padded = "0x" + hex(bucket[1])[2:].rjust(self.L//8, '0')
-
-            key = "{}~{}".format(lower_padded, upper_padded)
-            pretty[key] = []
-
-            for node_contact, node_addr in self.table.buckets[bucket].items():
-                pretty_addr = "0x" + node_addr.addr.hex()
-                pretty_ip = node_contact.host + ":" + str(node_contact.port)
-                pretty[key].append((pretty_addr, pretty_ip))
-
-            pretty[key].sort()  # lex ordering naturally sorts addrs ascending
-
-        return pretty
+    def _get_local_addrs(self):
+        if self.local_peer is not None:
+            return self.local_peer.node_addrs
+        return []
 
     @staticmethod
-    def get_node_addrs(contact_info):
-        from app import peer
-        peer = peer.peer_tracker.get(contact_info)
-        if peer is None:
-            RoutingTable.log.warn("Tried to get node addrs for {contact} but peer_tracker has no corresponding record.", contact=contact_info)
-            return []
-        return peer.get_info(ADDRS)
-
-    @staticmethod
-    def bytes_to_int(node_addr):
+    def bytes_to_int(bytestring):
         n = 0
-        for byte in node_addr:
+        for byte in bytestring:
             n <<= 8
             n += byte
         return n
--- theseus/test/test_routing.py
+++ theseus/test/test_routing.py
@@ -2,7 +2,41 @@ from twisted.trial import unittest
 
 from theseus.routing import RoutingTable
 
+from theseus.nodeaddr import NodeAddress
+from theseus.constants import k
+
 
 class RoutingTests(unittest.TestCase):
-    def setUp(self):
-        self.table = RoutingTable()
+    def test_basic_inserts(self):
+        table = RoutingTable()
+        good_addrs = [bytes([i])*20 for i in range(k)]
+        for addr in good_addrs:
+            self.assertTrue(table.insert(None, NodeAddress(addr, None)))
+        self.assertFalse(table.insert(None, NodeAddress(b'\xFF'*20, None)))
+        self.assertEqual(good_addrs, [entry.node_addr.addr for entry in table.root.get_contents()])
+
+    def test_basic_splits(self):
+        class DummyPeer:
+            node_addrs = [NodeAddress(b'\x00'*20, None), NodeAddress(b'\xFF' + b'\x00'*19, None)]
+
+        table = RoutingTable(DummyPeer())
+        low_addrs = [bytes([i])*20 for i in range(k)]
+        for addr in low_addrs:
+            self.assertTrue(table.insert(None, NodeAddress(addr, None)))
+
+        high_addrs = [b'\xFF' + bytes([i])*19 for i in range(k)]
+        for addr in high_addrs:
+            self.assertTrue(table.insert(None, NodeAddress(addr, None)))
+
+        med_addrs = [b'\xA0\x00' + bytes([i])*18 for i in range(k)]
+        #med_addrs += [b'\x80\x00' + bytes([i])*18 for i in range(k)]
+
+        for addr in med_addrs:
+            self.assertTrue(table.insert(None, NodeAddress(addr, None)))
+
+        self.assertFalse(table.insert(None, NodeAddress(b'\x81'+bytes(19), None)))
+        self.assertFalse(table.insert(None, NodeAddress(b'\x82'+bytes(19), None)))
+        self.assertEqual(
+                low_addrs + med_addrs + high_addrs,
+                [entry.node_addr.addr for entry in table.root.get_contents()]
+                )
