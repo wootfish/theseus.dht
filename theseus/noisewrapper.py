37384e989fb8e8e8015bb8ebb5cb46ff8039519d
Eli Sohl <elisohl@gmail.com>
2018-09-26 01:53:42 -0700

    tweaked noisewrapper log message

--- theseus/noisewrapper.py
+++ theseus/noisewrapper.py
@@ -116,7 +116,8 @@ class NoiseWrapper(ProtocolWrapper):
                     self._process_message(data)
 
             except Exception as e:
-                self.log.error("Unexpected exception {e} caused by received data {data}", e=e, data=data)
+                self.log.error("Unexpected exception: {e}", e=e)
+                self.log.info("Exception caused by received data {data}", data=data)
                 self.log.info("{peer} - Terminating connection due to unexpected error.", peer=self._peer)
                 self.transport.loseConnection()
                 return
