26b2ae4ff8192fe8063a10785e6db71144eb5b56
Eli Sohl <elisohl@gmail.com>
2018-09-30 21:13:37 -0700

    ContactInfo dunder tweaks

--- theseus/contactinfo.py
+++ theseus/contactinfo.py
@@ -7,16 +7,17 @@ class ContactInfo:
         self.port = port
         self.key = key
 
+    def __key(self):
+        return (self.host, self.port, self.key)
+
     def __hash__(self):
-        return hash((self.host, self.port, self.key))
+        return hash(self.__key())
 
     def __eq__(self, other):
-        if other.__class__ is self.__class__:
-            return (self.host, self.port, self.key) == (other.host, other.port, other.key)
-        return NotImplemented
+        return isinstance(other, self.__class__) and self.__key() == other.__key()
 
     def __repr__(self):
-        return "ContactInfo({}, {}, {})".format(self.host, self.port, self.key)
+        return "ContactInfo({}, {}, {})".format(*self.__key())
 
     def as_bytes(self):
         ...  # TODO
