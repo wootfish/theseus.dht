ab2ec40d62d3983ecee6e92fb8437e8c5a938b93
Eli Sohl <elisohl@gmail.com>
2018-10-02 00:39:27 -0700

    cleaned up ContactInfo

--- theseus/contactinfo.py
+++ theseus/contactinfo.py
@@ -1,14 +1,29 @@
 from twisted.internet.address import IPv4Address
 
+from noise.functions import KeyPair25519
+
 
 class ContactInfo:
     def __init__(self, host, port, key):
+        # host: str, currently expected to be IPv4 in dotted form
+
+        # currently, key: KeyPair25519.from_public_bytes
+        # in future, key: noise.functions._KeyPair subclass  (hopefully)
+
+        # (key is not bytes because we don't want higher levels of abstraction
+        # absorbing the complexity of having to know what public key algorithm
+        # is in use -- _KeyPair can abstract this information away & provide a
+        # consistent interface)
+
+        if type(key) is bytes:
+            key = KeyPair25519.from_public_bytes(key)
+
         self.host = host
         self.port = port
         self.key = key
 
     def __key(self):
-        return (self.host, self.port, self.key)
+        return (self.host, self.port, self.key.public_bytes)
 
     def __hash__(self):
         return hash(self.__key())
@@ -18,13 +33,6 @@ class ContactInfo:
 
     def __repr__(self):
         return "ContactInfo({}, {}, {})".format(*self.__key())
-
-    def as_bytes(self):
-        ...  # TODO
-
-    @classmethod
-    def from_bytes(cls, bytestring):
-        ...  # TODO
 
     def get_addr(self):
         return IPv4Address("TCP", self.host, self.port)
