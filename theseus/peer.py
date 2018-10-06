2c561e60f68c93c01be67d844ccc2e1f3d94a19e
Eli Sohl <elisohl@gmail.com>
2018-10-06 03:39:35 -0700

    added note about minor bug to peer.py

--- theseus/peer.py
+++ theseus/peer.py
@@ -87,12 +87,11 @@ class PeerService(Service):
         self.routing_table.reload(new_addrs)  # TODO pass in full list of eligible peers?
         # TODO should we advertise this info change? probably, right?
 
-
         # run lookups for all addresses
         for addr in new_addrs:
             def cb(_):
                 self.log.debug("Addr lookup complete -- lookups: {lookups} ({addr} {addr_bytes})", lookups=self._addr_lookups, addr=addr.addr.hex(), addr_bytes=addr.addr)
-                self._addr_lookups.remove(d)
+                self._addr_lookups.remove(d)  # FIXME this line errbacks sometimes for some reason. can we refactor this whole mess to make that issue moot?
             d = self.do_lookup(addr.addr)
             self._addr_lookups.append(d)
             d.addCallback(cb)
