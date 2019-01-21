da3dd519e6e730fb5bf763413a43bf19e86b2df4
Eli Sohl <elisohl@gmail.com>
2019-01-20 19:11:14 -0500

    added StatsTracker size estimate support

--- theseus/statstracker.py
+++ theseus/statstracker.py
@@ -2,14 +2,18 @@ from random import SystemRandom
 from collections import deque
 from time import clock
 
+from twisted.logger import Logger
+
 from .errors import NotEnoughLookupsError
 from .constants import k, L
 
 
 class StatsTracker:
+    log = Logger()
     _rand = SystemRandom
     _clock = clock
     time_window = 3600
+    min_sample_size = 3
 
     def __init__(self, local_peer):
         self.measurements = deque(maxlen=1024)
@@ -34,28 +38,37 @@ class StatsTracker:
             self.measurements.popleft()
 
     def get_size(self):
+        self.log.debug("Attempting network size estimation.")
         self.trim_old_measurements()
-        if len(self.measurements) < 10:
-            raise NotEnoughLookupsError()
-
         sample_size = len(self.measurements)
+        if sample_size < self.min_sample_size:
+            self.log.debug("Not enough samples for size estimate ({s}/{m})", s=sample_size, m=self.min_sample_size)
+            raise NotEnoughLookupsError
+
+        self.log.debug("Sample size: {s}", s=sample_size)
         d_i = [0]*k
-        for _, distances in self.measurements:
-            for i, d in distances:
+        for ts, distances in self.measurements:
+            for i, d in enumerate(distances):
                 d_i[i] += d / sample_size
-        r_i = [dist / 2**L for dist in d_i]
-        estimate = k*(k+1)*(2*k+1) / (6*sum(i*r for i, r in enumerate(r_i, 1)))
+        estimate = k*(k+1)*(2*k+1) / (6*sum(i*d/2**L for i, d in enumerate(d_i, 1)))
+        self.log.debug("Current network size estimate: {n}", n=estimate)
         return estimate
 
     def register_lookup(self, d, addr):
-        def cb(self, nodes):
+        def cb(nodes):
+            self.log.debug("Callback on lookup for {addr} starting.", addr=addr.hex())
             distances = sorted(
                 self._xor(addr, routing_entry.node_addr.addr)
                 for routing_entry in nodes
             )
             self.measurements.append((self._clock(), distances))
             self.trim_old_measurements()
+            try:
+                n = self.get_size()
+                self.log.debug("Lookup callback hit. Current size estimate: {n}", n=n)
+            except NotEnoughLookupsError:
+                self.log.debug("Not enough lookups to make a size estimate...")
             return nodes
 
+        self.log.debug("Registering lookup for {addr}", addr=addr.hex())
         d.addCallback(cb)
-
